34d5d1cfbe3f3b696a31924b3af566e2b544e690
vishal <kannavish329@gmail.com>
2025-07-17 18:40:33 +0530

    fix: fix tests

--- programs/healthlock/src/instructions/upload_health_record.rs
+++ programs/healthlock/src/instructions/upload_health_record.rs
@@ -22,12 +22,13 @@ pub fn upload_health_record(
     let health_record = &mut ctx.accounts.health_record;
     let current_timestamp = Clock::get()?.unix_timestamp;
 
-    user_vault.owner = ctx.accounts.owner.key();
-    user_vault.record_ids = Vec::new();
-    user_vault.created_at = current_timestamp;
-    user_vault.is_active = true;
-
-
+    if !user_vault.is_active {
+        user_vault.owner = ctx.accounts.owner.key();
+        user_vault.record_ids = Vec::new();
+        user_vault.created_at = current_timestamp;
+        user_vault.is_active = true;
+    }
+   
     health_record.owner = ctx.accounts.owner.key();
     health_record.record_id = record_counter.record_id;
     health_record.encrypted_data = encrypted_data;
